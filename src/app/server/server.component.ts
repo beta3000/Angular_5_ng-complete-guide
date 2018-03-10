0d1f5413ee77889d3a29888cfd8f9a7fb1abbc7e
beta3000 <atlas.aresco@gmail.com>
2018-03-10 11:13:52 -0500

    String Interpolation.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,11 @@ import {Component} from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverId = 10;
+  private _serverStatus = 'offline';
 
+
+  get serverStatus(): string {
+    return this._serverStatus;
+  }
 }
